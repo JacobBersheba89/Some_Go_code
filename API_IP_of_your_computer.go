4764aebf0b539545a519af722d303922fa4cf851
JacobP <jakubpawlas89@gmail.com>
2024-11-24 14:36:12 +0100

    Add files via upload
    ..have to finish this go code.. is not ready

---
+++ API_IP_of_your_computer.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// Struktura pro parsování odpovědi z API
+type ApiResponse struct {
+	IP        string  `json:"ip"`
+	Country   string  `json:"country_name"`
+	Region    string  `json:"region_name"`
+	City      string  `json:"city"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+func main() {
+	// API klíč (vlož svůj vlastní klíč zde)
+	apiKey := "dfde5e0a-dc8d-4ed1-aaae-cad3d2f01f6c" // Změň na svůj klíč z apiip.net
+	apiURL := fmt.Sprintf("https://apiip.net/api?key=%s", apiKey)
+
+	// Odeslat HTTP GET požadavek
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		fmt.Println("Chyba při získávání veřejné IP adresy:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Kontrola stavu odpovědi
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Chyba: server vrátil status %d\n", resp.StatusCode)
+		return
+	}
+
+	// Parsování JSON odpovědi
+	var data ApiResponse
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		fmt.Println("Chyba při parsování JSON odpovědi:", err)
+		return
+	}
+
+	// Výstup získaných dat
+	fmt.Println("Veřejná IP adresa počítače je:", data.IP)
+	fmt.Println("Země:", data.Country)
+	fmt.Println("Region:", data.Region)
+	fmt.Println("Město:", data.City)
+	fmt.Printf("Poloha: %.2f, %.2f\n", data.Latitude, data.Longitude)
+}
